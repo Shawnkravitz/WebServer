9ef8b317e6da4fa21d69f8a3dea1735048ce5b7c
andy1058 <andy1058@gmail.com>
2018-01-25 19:43:25 -0500

    Adding spring framework

---
+++ src/main/java/com/homeautomation/webserver/Response.java
@@ -0,0 +1,19 @@
+package com.homeautomation.webserver;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class Response {
+
+    @Autowired
+    private NodeRepository repository;
+
+
+
+    public Response(String name){
+
+    }
+
+
+}
