c77f5ea446b1d77ba2461a8f6e11ca41f704964c
James Olds <james@oldsmail.com>
2018-01-31 20:27:34 -0600

    Remove unused imports Add gitignore

--- src/main/java/com/homeautomation/webserver/ResponseController.java
+++ src/main/java/com/homeautomation/webserver/ResponseController.java
@@ -2,14 +2,9 @@ package com.homeautomation.webserver;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mongodb.util.JSON;
-import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
