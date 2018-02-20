f03c02774a59a0fdd5694320b09d785c5bf2d432
James Olds <james@oldsmail.com>
2018-02-19 22:59:57 -0600

    Update (stop)start_webserver.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,5 @@
+cp /home/ubuntu/webserver.service /etc/systemd/system/
+
 systemctl daemon-reload
 systemctl enable webserver.service
 systemctl start webserver.service
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,1 +1,5 @@
 systemctl stop webserver.service
+rm /etc/systemd/system/webserver.service
+
+cp /home/ubuntu/webserver.service /etc/systemd/system/
+systemctl daemon-reload
