665eb5099093cbe56c4c6dd0548424b0d88adb7a
James Olds <james@oldsmail.com>
2018-02-19 23:24:13 -0600

    Switch to Blue/green deployment

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,3 @@
-cp /home/ubuntu/webserver.service /etc/systemd/system/
-
 systemctl daemon-reload
 systemctl enable webserver.service
 systemctl start webserver.service
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,6 +1,1 @@
 systemctl stop webserver.service
-rm /etc/systemd/system/webserver.service
-
-cp /home/ubuntu/webserver.service /etc/systemd/system/
-rm /home/ubuntu/webserver-0.0.1-SNAPSHOT.jar
-systemctl daemon-reload
