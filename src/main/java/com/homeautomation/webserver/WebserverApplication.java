a2841ea3480c9002c093609369b1a7f9bb34ceac
James Olds <james@oldsmail.com>
2018-02-22 11:27:44 -0600

    Modify buildspec to attempt to modify myServerURL variable on release to production

--- src/main/java/com/homeautomation/webserver/WebserverApplication.java
+++ src/main/java/com/homeautomation/webserver/WebserverApplication.java
@@ -12,33 +12,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class WebserverApplication implements CommandLineRunner {
 
-	@Autowired
-	private NodeRepository repository;
-
-	@Autowired
-	private UserRepository userRepository;
-
-	// main program
 	public static void main(String[] args) {
 		SpringApplication.run(WebserverApplication.class, args);
 	}
 
 	public void run(String... args) throws Exception {
 
-
-		// delete all users and nodes
-		this.repository.deleteAll();
-		this.userRepository.deleteAll();
-
-		// save a couple of customers
-		this.repository.save(new Node("bedroom1", "true", "bedroom light"));
-		this.repository.save(new Node("bedroom2", "false", "second bedroom"));
-		this.repository.save(new Node("garagedoor", "true", "garage door"));
-
-
-		// save a few example usernames and passwords
-		this.userRepository.save(new User("JimHalpert", "iheartpam"));
-		this.userRepository.save(new User("MichaelScott", "thatswhatshesaid"));
-		this.userRepository.save(new User("DwightSchrute", "beets"));
 	}
 }
