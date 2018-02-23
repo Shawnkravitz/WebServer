a6849513694b5a4d2a9be616359941e9e0cc904d
James Olds <oldsj@users.noreply.github.com>
2018-02-23 08:28:37 -0600

    Update SecurityConstants comments

--- src/main/java/com/homeautomation/webserver/security/SecurityConstants.java
+++ src/main/java/com/homeautomation/webserver/security/SecurityConstants.java
@@ -2,7 +2,7 @@ package com.homeautomation.webserver.security;
 
 public class SecurityConstants {
     public static final String SECRET = "23908asfkKLJasdjag0425a";
-    public static final long EXPIRATION_TIME = 600000; // 10 days
+    public static final long EXPIRATION_TIME = 600000; // 10 minutes
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/users/sign-up";
